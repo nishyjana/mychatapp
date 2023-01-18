4853e97f036a137c9edb2969deeaa71f81e77dde
Nishanthan Janarthanarajah <nishanthanj@conceptvines.com>
2023-01-19 00:15:37 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 1;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -55,13 +55,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
-            ),
-             const Text(
-              'You have pushed the above button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline3,
             ),
           ],
         ),
