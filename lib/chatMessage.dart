dc2ab01f57a81012c1e28f5292c12bf640a7ca5e
Nishanthan Janarthanarajah <nishanthanj@conceptvines.com>
2023-01-24 17:29:59 +0530

    init

--- lib/chatMessage.dart
+++ lib/chatMessage.dart
@@ -10,22 +10,57 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(sender)
-            .text
-            .subtitle1(context)
-            .make()
-            .box
-            .color(sender == "user" ? Vx.cyan400 : Vx.green400)
-            .p16
-            .rounded
-            .alignCenter
-            .makeCentered(),
+      textDirection: TextDirection.rtl,
+      children: <Widget>[
         Expanded(
-          child: text.trim().text.bodyText1(context).make().px8().py2(),
-        ),
+            child: sender == 'user'
+                ? Text(text)
+                    .text
+                    .subtitle1(context)
+                    .make()
+                    .box
+                    .rounded
+                    .alignCenterRight
+                    .py12
+                    .px12
+                    .height(50)
+                    .color(sender == "user" ? Vx.cyan400 : Vx.green400)
+                    .make()
+                    .p12()
+                : Text(text)
+                    .text
+                    .subtitle1(context)
+                    .make()
+                    .box
+                    .rounded
+                    .alignCenterLeft
+                    .py20
+                    .px12
+                    .height(100)
+                    .color(sender == "user" ? Vx.cyan400 : Vx.green400)
+                    .make()
+                    .p12()
+            // text.trim().text.bodyText1(context).make().px8().py2(),
+            ),
+      
+        if (sender != 'user') Icon(Icons.smart_toy_rounded).iconSize(50).iconColor(Colors.green)
       ],
-    ).py8();
+    );
   }
 }
+
+
+
+
+
+  // if (sender == 'user')
+        //   Text(sender[0].toUpperCase())
+        //       .text
+        //       .subtitle1(context)
+        //       .make()
+        //       .box
+        //       .color(sender == "user" ? Vx.cyan400 : Vx.green400)
+        //       .p16
+        //       .roundedFull
+        //       .alignCenter
+        //       .makeCentered(),
